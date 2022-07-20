b9549b42e1484e5dd444a48aafd03d3a70fce637
linh97abc <linh97abc@gmail.com>
2022-07-21 00:53:46 +0700

    update sample

--- main.c
+++ main.c
@@ -1,11 +1,97 @@
-
 #include <stdio.h>
 
-extern void wiz_lowlevel_setup(void);
+void wiz_lowlevel_setup(void);
+
+#include "wizchip_conf.h"
+#include "ioLibrary_Driver/Application/loopback/loopback.h"
+
+
+/* Buffer */
+#define ETHERNET_BUF_MAX_SIZE (1024 * 2)
+
+/* Socket */
+#define SOCKET_LOOPBACK 0
+
+/* Port */
+#define PORT_LOOPBACK 5000
+
+/**
+ * ----------------------------------------------------------------------------------------------------
+ * Variables
+ * ----------------------------------------------------------------------------------------------------
+ */
+/* Network */
+static wiz_NetInfo g_net_info =
+    {
+        .mac = {0x00, 0x08, 0xDC, 0x12, 0x34, 0x56}, // MAC address
+        .ip = {192, 168, 11, 2},                     // IP address
+        .sn = {255, 255, 255, 0},                    // Subnet Mask
+        .gw = {192, 168, 11, 1},                     // Gateway
+        .dns = {8, 8, 8, 8},                         // DNS server
+        .dhcp = NETINFO_STATIC                       // DHCP enable/disable
+};
+
+/* Loopback */
+static uint8_t g_loopback_buf[ETHERNET_BUF_MAX_SIZE] = {
+    0,
+};
+
+/* Network */
+void network_initialize(wiz_NetInfo net_info)
+{
+    ctlnetwork(CN_SET_NETINFO, (void *)&net_info);
+}
+
+void print_network_information(wiz_NetInfo net_info)
+{
+    uint8_t tmp_str[8] = {
+        0,
+    };
+
+    ctlnetwork(CN_GET_NETINFO, (void *)&net_info);
+    ctlwizchip(CW_GET_ID, (void *)tmp_str);
+
+    if (net_info.dhcp == NETINFO_DHCP)
+    {
+        printf("====================================================================================================\n");
+        printf(" %s network configuration : DHCP\n\n", (char *)tmp_str);
+    }
+    else
+    {
+        printf("====================================================================================================\n");
+        printf(" %s network configuration : static\n\n", (char *)tmp_str);
+    }
+
+    printf(" MAC         : %02X:%02X:%02X:%02X:%02X:%02X\n", net_info.mac[0], net_info.mac[1], net_info.mac[2], net_info.mac[3], net_info.mac[4], net_info.mac[5]);
+    printf(" IP          : %d.%d.%d.%d\n", net_info.ip[0], net_info.ip[1], net_info.ip[2], net_info.ip[3]);
+    printf(" Subnet Mask : %d.%d.%d.%d\n", net_info.sn[0], net_info.sn[1], net_info.sn[2], net_info.sn[3]);
+    printf(" Gateway     : %d.%d.%d.%d\n", net_info.gw[0], net_info.gw[1], net_info.gw[2], net_info.gw[3]);
+    printf(" DNS         : %d.%d.%d.%d\n", net_info.dns[0], net_info.dns[1], net_info.dns[2], net_info.dns[3]);
+    printf("====================================================================================================\n\n");
+}
+
 
 int main()
 {
+    /* Initialize */
+    int retval = 0;
+
     wiz_lowlevel_setup();
-    puts(__func__);
-    return 0;
+    network_initialize(g_net_info);
+
+    /* Get network information */
+    print_network_information(g_net_info);
+
+    /* Infinite loop */
+    while (1)
+    {
+        /* TCP server loopback test */
+        if ((retval = loopback_tcps(SOCKET_LOOPBACK, g_loopback_buf, PORT_LOOPBACK)) < 0)
+        {
+            printf(" Loopback error : %d\n", retval);
+
+            while (1)
+                ;
+        }
+    }
 }
